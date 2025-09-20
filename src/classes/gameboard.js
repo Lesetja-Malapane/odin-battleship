ca87fbd5810c7174de958dfc657495d60b5ff055
Lesetja <kmalapane155@gmail.com>
2025-09-20 18:40:02 +0200

    Edit: Use list to store ships and Ship obj as objects, improved code

--- src/classes/gameboard.js
+++ src/classes/gameboard.js
@@ -2,37 +2,44 @@ import Ship from "./ship";
 
 export default class Gameboard {
   constructor() {
-    this.ships = {};
+    this.ships = [];
   }
 
   receiveAttack(coordinates) {
-    console.log(this.ships.values);
-    this.ships.keys.forEach((ship) => {
-      if (this.ships[ship] == [coordinates.x, coordinates.y]) {
-        ship.hit();
+    for (const { ship, coordinates: coords } of this.ships) {
+      for (let i = 0; i < coords.length; i++) {
+        if (coords[i][0] === coordinates.x && coords[i][1] === coordinates.y) {
+          ship.hit();
+          return true;
+        }
       }
-    });
+    }
+    return false;
   }
 
   placeShip(coordinates, shipLength, direction) {
-    const ship = new Ship();
+    const ship = new Ship(shipLength);
     let x = coordinates.x;
     let y = coordinates.y;
     let shipCoordinates = [];
 
     if (direction.toLowerCase() === "horizontal") {
       for (let i = 0; i < shipLength; i++) {
-        x += i;
         shipCoordinates.push([x, y]);
+        x += 1;
       }
     } else if (direction.toLowerCase() === "vertical") {
       for (let i = 0; i < shipLength; i++) {
-        y += i;
         shipCoordinates.push([x, y]);
+        y += 1;
       }
     } else {
-      throw new Error("Worng ship direction");
+      throw new Error("Wrong ship direction");
     }
-    this.ships[ship] = shipCoordinates;
+    this.ships.push({ ship, coordinates: shipCoordinates });
+  }
+
+  shipsOnBoard() {
+    return this.ships;
   }
 }
