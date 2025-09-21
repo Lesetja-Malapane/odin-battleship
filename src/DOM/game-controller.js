cedd7902c7bbdc6c6ffe69cce4e0e9f709ae0d0e
Lesetja <kmalapane155@gmail.com>
2025-09-21 12:50:10 +0200

    Edit: Move game code to a controller file Addition: create game-controller to handle game logic

---
+++ src/DOM/game-controller.js
@@ -0,0 +1,50 @@
+import "./renderGameboard";
+import renderPlayerShips from "./renderPlayerShips";
+import Player from "../classes/player";
+
+const player1 = new Player("player1");
+const player2 = new Player("player2");
+
+const opponentBoard = document.getElementById("opponentBoard");
+const opponentCells = opponentBoard.querySelectorAll(".cell");
+
+function addShipToPlayerBoard(player, coordinates, direction, shipLength) {
+  player.gameboard.placeShip(coordinates, shipLength, direction);
+}
+
+addShipToPlayerBoard(player1, { x: 0, y: 0 }, "horizontal", 4);
+addShipToPlayerBoard(player1, { x: 3, y: 4 }, "vertical", 3);
+addShipToPlayerBoard(player1, { x: 2, y: 2 }, "horizontal", 2);
+
+addShipToPlayerBoard(player2, { x: 1, y: 1 }, "horizontal", 4);
+addShipToPlayerBoard(player2, { x: 4, y: 3 }, "vertical", 3);
+addShipToPlayerBoard(player2, { x: 7, y: 7 }, "horizontal", 2);
+
+renderPlayerShips(player1);
+
+// const playerCoords = player1.gameboard.allShipsCoordinates()
+const opponentData = player2.gameboard;
+
+export default function game() {
+  // player's turn
+  opponentCells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      const cellCoord = [
+        Number.parseInt(cell.dataset.x),
+        Number.parseInt(cell.dataset.y),
+      ];
+      const opponentCoords = opponentData.allShipsCoordinates();
+
+      if (
+        opponentCoords.some(
+          (coord) => coord[0] === cellCoord[0] && coord[1] === cellCoord[1],
+        )
+      ) {
+        cell.classList.add("hit");
+      }
+    });
+  });
+  console.log(opponentData);
+}
+
+game();
--- src/index.js
+++ src/index.js
@@ -1,24 +1,2 @@
 import "./style.css";
-import "./DOM/renderGameboard";
-import renderPlayerShips from "./DOM/renderPlayerShips";
-
-import Player from "./classes/player";
-// import Ship from "./classes/ship";
-// import Gameboard from "./classes/gameboard"
-
-const player1 = new Player("player1");
-const player2 = new Player("player2");
-
-function addShipToPlayerBoard(player, coordinates, direction, shipLength) {
-  player.gameboard.placeShip(coordinates, shipLength, direction);
-}
-
-addShipToPlayerBoard(player1, { x: 0, y: 0 }, "horizontal", 4);
-addShipToPlayerBoard(player1, { x: 3, y: 4 }, "vertical", 3);
-addShipToPlayerBoard(player1, { x: 2, y: 2 }, "horizontal", 2);
-
-addShipToPlayerBoard(player2, { x: 1, y: 1 }, "horizontal", 4);
-addShipToPlayerBoard(player2, { x: 4, y: 3 }, "vertical", 3);
-addShipToPlayerBoard(player2, { x: 7, y: 7 }, "horizontal", 2);
-
-renderPlayerShips(player1);
+import "./DOM/game-controller";
