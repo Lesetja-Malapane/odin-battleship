b69c2bf7e451062a3d7279183ecc19b68a1e3952
Lesetja <lemalapjhb024@student.wethinkcode.co.za>
2025-09-22 15:22:07 +0200

    computer plays update

--- src/DOM/game-controller.js
+++ src/DOM/game-controller.js
@@ -8,6 +8,9 @@ const player2 = new Player("player2");
 const opponentBoard = document.getElementById("opponentBoard");
 const opponentCells = opponentBoard.querySelectorAll(".cell");
 
+const playerBoard = document.getElementById("playerBoard");
+const playerCells = playerBoard.querySelectorAll(".cell");
+
 function addShipToPlayerBoard(player, coordinates, direction, shipLength) {
   player.gameboard.placeShip(coordinates, shipLength, direction);
 }
@@ -22,7 +25,7 @@ addShipToPlayerBoard(player2, { x: 7, y: 7 }, "horizontal", 2);
 
 renderPlayerShips(player1);
 
-// const playerCoords = player1.gameboard.allShipsCoordinates()
+const playerData = player1.gameboard;
 const opponentData = player2.gameboard;
 
 export default function game() {
@@ -43,10 +46,45 @@ export default function game() {
         cell.classList.add("hit");
       } else {
         cell.classList.add("miss");
+        let turn = computerTurn();
+        while (turn) {
+          computerPlays();
+          turn = computerTurn();
+        }
       }
     });
   });
   console.log(opponentData);
+}
+
+function computerTurn() {
+  const playerCoords = playerData.allShipsCoordinates();
+
+  setTimeout(() => {
+    console.log("Computer's Turn");
+    const computerChoice = [
+      Math.floor(Math.random() * 10),
+      Math.floor(Math.random() * 10),
+    ];
+
+    const targetDiv = Array.from(playerCells).find(
+      (div) =>
+        Number(div.dataset.x) === computerChoice[0] &&
+        Number(div.dataset.y) === computerChoice[1],
+    );
+    if (
+      playerCoords.some(
+        (coord) =>
+          coord[0] === computerChoice[0] && coord[1] === computerChoice[1],
+      )
+    ) {
+      targetDiv.classList.add("hit");
+      return true;
+    } else {
+      if (targetDiv) targetDiv.classList.add("miss");
+      return false;
+    }
+  }, 2000);
 }
 
 game();
